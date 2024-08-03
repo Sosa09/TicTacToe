549962e37916067402584b24c62ef48d25bb669f
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-08-03 23:55:42 +0200

    imlemented game instructions

--- TicTacToe/UIExperience.cs
+++ TicTacToe/UIExperience.cs
@@ -13,15 +13,28 @@ namespace TicTacToe
     /// </summary>
     public static class UIExperience
     {
-        static int[,] grid = new int[4, 4];
+
         public static void InitializationInterface()
         {
             Console.WriteLine("Hello Please enter your name to start to play: ");
         }
 
-        public static void DisplayGamerInfo()
+        private static string _gameInstrcution()
         {
-            Console.WriteLine($"Name: ");
+            return "The grid is composed of 3 rows and 3 columns each containing a header from 1 to 3.\n" +
+                    "To place yor value in a certan plaace of the grid begin with the row and finalize with the column.\n " +
+                    "FOR EXAMPLE\n" +
+
+                    " 1 2 3" +
+                    "1" +
+                    "2" +
+                    "3" +
+                    "let's say you want to pla ce your char on the middeltop so you start with row 1 press enter then column 2 ";
+        }
+        private static void DisplayGamerInfo()
+        {
+            Console.WriteLine(_gameInstrcution();
+            Console.WriteLine($"Hello: ");
         }
         public static void DesignGrid(string Headers)
         {
